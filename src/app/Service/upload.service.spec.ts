cff2d94a24dd071d9e4df54f27edba9411b6a118
Nileshpg <snilesh789@gmail.com>
2022-08-09 11:48:10 +0530

    [wip ]test

--- src/app/Service/upload.service.spec.ts
+++ src/app/Service/upload.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import {HttpClientModule} from '@angular/common/http';
 import { UploadService } from './upload.service';
 
 describe('UploadService', () => {
   let service: UploadService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[HttpClientModule],
+    });
     service = TestBed.inject(UploadService);
   });
 
