ba9ce4537191024471349307067024100d2a00a5
Nileshpg <snilesh789@gmail.com>
2022-08-10 11:34:56 +0530

    [Readme]

---
+++ README.md
@@ -0,0 +1,15 @@
+# Upload File
+
+This project is about Video File
+
+# Project Prerequisite
+
+1. Angular CLI v13.0.0
+2. Node.js v16.16.0
+
+
+# Adding Component to your project
+1. Copy App module [folder]() to your project
+2. Add file upload to your module
+3. Use Service in your compoent
+4. See [documentation]() for more details
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { UploadService } from './Service/upload.service';
@@ -9,7 +12,11 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        BrowserModule,
+        HttpClientModule,
+        ReactiveFormsModule,
+        FormsModule
       ],
       declarations: [
         AppComponent
@@ -38,18 +45,8 @@ describe('AppComponent', () => {
   it(`should have as title 'Upload'`, () => {
     expect(app.title).toEqual('Upload');
   });
-  it('upload',()=>{
-    // const mockFile = new File([''], 'filename', { type: 'text/pdf' });
-    // const mockEvt = { target: { files: [mockFile] } };
-    // app.onSelectFile(mockEvt)
-    // expect(app.file).toEqual(mockEvt.target.files[0])
-    // app.upload()
-  })
-  it('upload without file',()=>{
-    app.upload()
-  })
+
   it ('onSelectFile ', () =>{ it ('On file change', () =>{
-    
     
     app.upload()
   }) 
