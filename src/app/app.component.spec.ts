da29020d51ccd04739cc57dde745148d5bf20986
Deep1218 <deeppatel18dec@gmail.com>
2022-08-17 12:30:39 +0530

    [feature] unit testing completed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { FormsService } from './service/forms.service';
@@ -11,17 +12,53 @@ let mockFormsService = jasmine.createSpyObj('FormsService', [
 ]);
 
 describe('AppComponent', () => {
+  let app: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule, ReactiveFormsModule],
       declarations: [AppComponent],
-      providers: [{ provide: FormsService, useValue: mockFormsService }],
+      providers: [{ provider: FormsService, useValue: mockFormsService }],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+  });
+
+  it('should create app', () => {
+    fixture.detectChanges();
     expect(app).toBeTruthy();
+  });
+
+  it('should set card form on create', () => {
+    fixture.detectChanges();
+    expect(app.cardForm.value).toEqual({
+      cardNumber: '',
+      expiryDate: '',
+      cvvCode: '',
+      cardHolderName: '',
+    });
+  });
+
+  it('should call method onSubmit when form submit', () => {
+    app.cardForm.patchValue({
+      cardHolderName: 'Deep Patel',
+      cardNumber: '4555 8555 6555 9555',
+      cvvCode: '465',
+      expiryDate: '2025-08-28',
+    });
+
+    fixture.debugElement
+      .query(By.css('form'))
+      .triggerEventHandler('ngSubmit', null);
+
+    expect(app.formData).toEqual({
+      cardHolderName: 'Deep Patel',
+      cardNumber: '4555 8555 6555 9555',
+      cvvCode: '465',
+      expiryDate: '2025-08-28',
+    });
   });
 });
