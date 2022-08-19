d3a49bf1640c0c4a9f8700b46af707428a6d2009
Nileshpg <snilesh789@gmail.com>
2022-08-19 12:58:53 +0530

    completed sonar testing and also update the bootstrap

--- src/app/components/instagram-layout/instagram-layout.component.spec.ts
+++ src/app/components/instagram-layout/instagram-layout.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { InstagramLayoutComponent } from './instagram-layout.component';
+
 
 describe('InstagramLayoutComponent', () => {
   let component: InstagramLayoutComponent;
@@ -8,7 +10,8 @@ describe('InstagramLayoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InstagramLayoutComponent ]
+      declarations: [ InstagramLayoutComponent ],
+      imports: [ReactiveFormsModule]
     })
     .compileComponents();
 
