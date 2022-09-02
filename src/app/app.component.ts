54a6936926dd1af2f6812098389c1f6459ca1c3b
Nileshpg <snilesh789@gmail.com>
2022-09-02 12:28:47 +0530

    [completed]  Sonar Testing and Unit testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
   data:any;
