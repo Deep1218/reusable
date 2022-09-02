bfa55d0f2bd4d28ae892ff423394dfdf00b4ec38
Nileshpg <snilesh789@gmail.com>
2022-09-02 12:35:35 +0530

    [chnge]

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-=======
 
-
-
-
->>>>>>> 0a1f75e4567370bedfad013625e4ca5f4f0089cd
